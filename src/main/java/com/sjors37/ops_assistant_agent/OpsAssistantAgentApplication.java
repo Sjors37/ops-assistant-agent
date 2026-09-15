e7570a973ea34834910698e7d9a5a92cb4fb5fd1
Sjors37 <sjorshekerman@live.nl>
2026-09-15 16:39:29 +0200

    initialize

---
+++ src/main/java/com/sjors37/ops_assistant_agent/OpsAssistantAgentApplication.java
@@ -0,0 +1,13 @@
+package com.sjors37.ops_assistant_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsAssistantAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsAssistantAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjors37/ops_assistant_agent/OpsAssistantAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjors37.ops_assistant_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsAssistantAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
